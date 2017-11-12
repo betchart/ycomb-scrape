206d9d7648ffe5604d019624628e175bd144947a
Burton Betchart <betchart@gmail.com>
2017-11-12 14:18:45 -0500

    factor out scraping from Main to ScrapeNewsYC

--- src/Main.hs
+++ src/Main.hs
@@ -1,80 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Text.HTML.Scalpel
 import Control.Applicative
 import Control.Monad
 
-data EntryYC = EntryYC { rank :: Int
-                       , title :: String
-                       , points :: Int
-                       , commented :: Int
-                       } deriving (Show)
-
+import ScrapeNewsYC
 
 main :: IO ()
 main = do
-  stuff <- scrapeURL "https://news.ycombinator.com" entriesYComb
-  maybe printError printHeadlines stuff
+  es <- scrapeURL "https://news.ycombinator.com" entriesYC
+  maybe printError
+        printEntries es
 
 printError :: IO()
 printError = putStrLn "ERROR: Could not scrape the URL!"
 
-printHeadlines :: [EntryYC] -> IO()
-printHeadlines hs = putStrLn $ unlines $ map show hs
-
-entriesYComb :: Scraper String [EntryYC]
-entriesYComb = do hdlns <- headlinesYC
-                  sbtxs <- subtextsYC
-                  return $ zipEntriesYC hdlns sbtxs
-
-zipEntriesYC :: [(Int, EntryYC)]  -- ^ Complete list of EntryYC
-             -> [(Int, EntryYC)]  -- ^ Partial list of EntryYC with update values
-             -> [EntryYC]
-zipEntriesYC [] _ = []
-zipEntriesYC xs [] = map snd xs
-zipEntriesYC (x:xs) (y:ys) =
-    case ix==iy of
-      True -> ez:(zipEntriesYC xs ys)
-      False -> ex:(zipEntriesYC xs (y:ys))
-    where ez = EntryYC (rank ex) (title ex) (points ey) (commented ey)
-          ex = snd x
-          ey = snd y
-          ix = fst x
-          iy = fst y
-
-headlinesYC :: Scraper String [(Int, EntryYC)]
-headlinesYC = chroots ("tr" @: [hasClass "athing"]) $ do
-                idval <- attr "id" $ "tr" @: [hasClass "athing"]
-                sRank <- text $ "span" @: [hasClass "rank"]
-                sTitle <- text $ "a"  @: [hasClass "storylink"]
-                return (read idval ::Int,
-                        EntryYC (nRank sRank) sTitle 0 0)
-
-subtextsYC :: Scraper String [(Int, EntryYC)]
-subtextsYC = chroots ("td" @: [hasClass "subtext"]) $ do
-               href <- attr "href" $ "span" @: [hasClass "age"] // "a"
-               sPoints <- text $ "span" @: [hasClass "score"]
-               links <- texts $ "a" -- comments are the fourth link
-               return (hrefToId href,
-                       EntryYC 0 "" (nPoints sPoints) (nComments $ last links))
-
-nRank :: String       -- ^ Has the form "NN."
-      -> Int          -- ^ NN parsed as int
-nRank s = read n :: Int
-    where n = reverse . drop 1 $ reverse s
-                    
-nComments :: String     -- ^ Has the form "NN\160comments" or "discuss"
-          -> Int        -- ^ NN parsed as int
-nComments "discuss" = 0
-nComments s = read n :: Int
-    where n = head $ words s
-
-nPoints :: String      -- ^ Has the form "NN points"
-        -> Int         -- ^ NN parsed as int
-nPoints s = read n :: Int
-    where n = head $ words s
-
-hrefToId :: String     -- ^ href has the form "item?id=NN"
-         -> Int        -- ^ NN parsed as int
-hrefToId h = read . drop 1 .snd $ break (=='=') h :: Int
+printEntries = putStrLn . unlines . map show
---
+++ src/ScrapeNewsYC.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+module ScrapeNewsYC ( EntryYC
+                    , entriesYC
+                    ) where
+
+import Text.HTML.Scalpel
+import Control.Applicative
+import Control.Monad
+    
+data EntryYC = EntryYC { rank :: Int
+                       , title :: String
+                       , points :: Int
+                       , commented :: Int
+                       } deriving (Show)
+
+entriesYC :: Scraper String [EntryYC]
+entriesYC = do hdlns <- headlinesYC
+               sbtxs <- subtextsYC
+               return $ zipEntriesYC hdlns sbtxs
+
+zipEntriesYC :: [(Int, EntryYC)]  -- ^ Complete list of EntryYC
+             -> [(Int, EntryYC)]  -- ^ Partial list of EntryYC with update values
+             -> [EntryYC]
+zipEntriesYC [] _ = []
+zipEntriesYC xs [] = map snd xs
+zipEntriesYC (x:xs) (y:ys) =
+    if ix == iy
+    then ez : zipEntriesYC xs ys
+    else ex : zipEntriesYC xs (y : ys)
+    where ez = EntryYC (rank ex) (title ex) (points ey) (commented ey)
+          (ix,ex) = x
+          (iy,ey) = y
+
+headlinesYC :: Scraper String [(Int, EntryYC)]
+headlinesYC = chroots ("tr" @: [hasClass "athing"]) $ do
+                idval <- attr "id" $ "tr" @: [hasClass "athing"]
+                sRank <- text $ "span" @: [hasClass "rank"]
+                sTitle <- text $ "a"  @: [hasClass "storylink"]
+                return (read idval ::Int,
+                        EntryYC (nRank sRank) sTitle 0 0)
+
+subtextsYC :: Scraper String [(Int, EntryYC)]
+subtextsYC = chroots ("td" @: [hasClass "subtext"]) $ do
+               href <- attr "href" $ "span" @: [hasClass "age"] // "a"
+               sPoints <- text $ "span" @: [hasClass "score"]
+               links <- texts "a" -- comments are the fourth link
+               return (hrefToId href,
+                       EntryYC 0 "" (nPoints sPoints) (nComments $ last links))
+
+
+nRank :: String       -- ^ Has the form "NN."
+      -> Int          -- ^ NN parsed as int
+nRank s = read n :: Int
+    where n = reverse . drop 1 $ reverse s
+                    
+nComments :: String     -- ^ Has the form "NN\160comments" or "discuss"
+          -> Int        -- ^ NN parsed as int
+nComments "discuss" = 0
+nComments s = read n :: Int
+    where n = head $ words s
+
+nPoints :: String      -- ^ Has the form "NN points"
+        -> Int         -- ^ NN parsed as int
+nPoints s = read n :: Int
+    where n = head $ words s
+
+hrefToId :: String     -- ^ href has the form "item?id=NN"
+         -> Int        -- ^ NN parsed as int
+hrefToId h = read . drop 1 .snd $ break (=='=') h :: Int
