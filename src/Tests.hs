45fd4e9f8a1fbd60543ad48675141916b82e67ba
Burton Betchart <betchart@gmail.com>
2017-11-13 02:26:02 -0500

    Six tests for requestedFilters, all passing

--- src/Tests.hs
+++ src/Tests.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 
 import Test.Framework
+import Data.List (sort,sortBy)
+import Data.Function (on)
 
 import ScrapeNewsYC
 import ProcessNewsYC (requestedFilters)
@@ -27,3 +29,21 @@ prop_fiveWordsOrFewer :: [(Int,String,Int,Int)] -> Bool
 prop_fiveWordsOrFewer abcds =
     all ((<=5).length.words.title) shorts
         where shorts = snd.requestedFilters.map makeEntryYC $ abcds
+
+prop_sortedByComments :: [(Int,String,Int,Int)] -> Bool
+prop_sortedByComments abcds = longComs == sort longComs
+        where longs = fst.requestedFilters.map makeEntryYC $ abcds
+              longComs = map commented longs
+
+prop_sortedByPoints :: [(Int,String,Int,Int)] -> Bool
+prop_sortedByPoints abcds = shortPts == sort shortPts
+        where shorts = snd.requestedFilters.map makeEntryYC $ abcds
+              shortPts = map points shorts
+
+prop_closure :: [(Int,String,Int,Int)] -> Bool
+prop_closure abcds = srt es == srt es'
+    where es = map makeEntryYC abcds
+          es' = a ++ b
+          (a,b) = requestedFilters es
+          srt = sortBy (compare `on` hackhash)
+          hackhash e = (rank e, title e, points e, commented e)
