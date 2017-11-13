9a32f9e802924bf2e5f9c034b212a9a2a84454a0
Burton Betchart <betchart@gmail.com>
2017-11-12 23:53:43 -0500

    add language from spec to document requestedFilters; switch to ascending sort

--- src/Main.hs
+++ src/Main.hs
@@ -26,23 +26,26 @@ processEntries es = do
 
   putStrLn $ "\nSubset of " ++ (show.length$ tLongsByComments) ++
                " entries with more than 5 words in title,\n" ++
-               "ordered by number of comments decreasing:\n"
+               "ordered by number of comments:\n"
   printEntries tLongsByComments
 
   putStrLn $ "\nSubset of " ++ (show.length$ tShortsByPoints) ++
                " entries with 5 or fewer words in title, \n" ++
-               "ordered by number of points decreasing:\n"
+               "ordered by number of points:\n"
   printEntries tShortsByPoints
 
     where (tLongsByComments, tShortsByPoints) = requestedFilters es
 
+-- |We want it to be able to perform a couple of filtering operations:
+--  * Filter all previous entries with more than five words in the title ordered by amount of comments first.
+--  * Filter all previous entries with less than or equal to five words in the title ordered by points.
 requestedFilters :: [EntryYC] -> ([EntryYC],[EntryYC])
 requestedFilters es = (tLongsByComments, tShortsByPoints)
     where
       titleG5 = (>5) . length . words . title
       (tLongs, tShorts) = partition titleG5 es
-      tLongsByComments =  reverse . sortBy (compare `on` commented) $ tLongs
-      tShortsByPoints =  reverse . sortBy (compare `on` points) $ tShorts
+      tLongsByComments =  sortBy (compare `on` commented) $ tLongs
+      tShortsByPoints =  sortBy (compare `on` points) $ tShorts
 
 printEntries :: [EntryYC] -> IO ()
 printEntries = putStrLn . unlines . map show
